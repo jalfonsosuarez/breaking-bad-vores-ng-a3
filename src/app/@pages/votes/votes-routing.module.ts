cd0d037a86be8c1fcc0d4a4ccbfd4064bb41a98d
= <=>
2021-10-30 18:33:55 +0200

    Angular project generation, pages & lacyload

---
+++ src/app/@pages/votes/votes-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { VotesComponent } from './votes.component';
+
+const routes: Routes = [
+  { path: '', component: VotesComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class VotesRoutingModule { }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', loadChildren: () => import( './@pages/characters/characters.module' ).then( (m) => m.CharactersModule) },
+  { path: 'votes', loadChildren: () => import( './@pages/votes/votes.module' ).then( (m) => m.VotesModule) },
+  { path: 'details/:id', loadChildren: () => import( './@pages/details/details.module' ).then( (m) => m.DetailsModule) },
+  { path: '**', redirectTo: '', pathMatch: 'full' },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    useHash: true,
+    scrollPositionRestoration: 'enabled'
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
